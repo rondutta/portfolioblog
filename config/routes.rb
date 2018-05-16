3fd9b145049f369306ecca61d144262819d7ce07
Rayan <rondutta23@gmail.com>
2018-05-16 16:30:26 +0530

    configured custom routes for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except:[:show]
   get 'pages/home'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   resources :blogs
