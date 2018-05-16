dd8282eb8933d24d4d6a58bbc5b0a203c25d067f
Rayan <rondutta23@gmail.com>
2018-05-16 13:59:52 +0530

    configured new and index erb for portfolios controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,25 @@
 class PortfoliosController < ApplicationController
+
   def index
     @portfolios = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def show
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio Post Uploaded.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
