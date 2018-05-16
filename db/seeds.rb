2a540a3d6b03c591b5d8f8dc45e5bf86455ae339
Rayan <rondutta23@gmail.com>
2018-05-16 11:32:36 +0530

    added sample data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+          title: "My Blig Post #{blog}",
+          body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                 when an unknown printer took a galley of type and scrambled it to make a type
+                 specimen book. It has survived not only five centuries, but also the leap
+                 into electronic typesetting, remaining essentially unchanged.
+                 It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                 passages, and more recently with desktop publishing software like Aldus PageMaker
+                 including versions of Lorem Ipsum"
+          )
+end
+
+5.times do |skill|
+  Skill.create(
+           title: "Ruby on Rails #{skill}",
+           percentage: 20
+  )
+end
