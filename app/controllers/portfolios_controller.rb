378e907603a2225c8f6f208689912368b290de4a
Rayan <rondutta23@gmail.com>
2018-05-16 14:37:14 +0530

    modified controllers from portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,6 +11,10 @@ class PortfoliosController < ApplicationController
   def show
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
     respond_to do |format|
@@ -18,6 +22,19 @@ class PortfoliosController < ApplicationController
         format.html { redirect_to portfolios_path, notice: 'Portfolio Post Uploaded.' }
       else
         format.html { render :new }
+      end
+    end
+  end
+
+  def update
+
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
       end
     end
   end
