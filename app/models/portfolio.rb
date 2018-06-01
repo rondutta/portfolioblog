746b5c8a6e0795adba370f839b3292a8953d9b75
Rayan <rondutta23@gmail.com>
2018-06-02 00:08:30 +0530

    implemented default values for images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,4 +4,9 @@ class Portfolio < ApplicationRecord
     where(subtitle: "React JS")
   end
   scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
+  after_initialize :set_defaults
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
 end
